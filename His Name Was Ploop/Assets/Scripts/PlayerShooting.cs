9adcb570985a15d7db285496ac45a55f319c7d64
Sweisian <rswei888@gmail.com>
2019-01-17 16:48:40 -0600

    added projectile shooting

---
+++ His Name Was Ploop/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 myPos2D = (Vector2)transform.position;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 direction = cursorInWorldPos - myPos2D;
+            direction.Normalize();
+            GameObject projectile = (GameObject)Instantiate(projectilePrefab, myPos2D, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+
+
+    }
+}
